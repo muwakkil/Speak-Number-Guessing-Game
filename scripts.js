8b0934499b8eee882e8dadba72ef719e461cc4c8
muwakkil <muwakkil@vcu.edu>
2025-10-14 19:43:57 -0400

    mods added

--- scripts.js
+++ scripts.js
@@ -1,5 +1,44 @@
 const msgEl = document.getElementById('msg');
 
+const canvas = document.getElementById('waveform');
+const ctx = canvas.getContext('2d');
+
+navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+  const audioCtx = new AudioContext();
+  const source = audioCtx.createMediaStreamSource(stream);
+  const analyser = audioCtx.createAnalyser();
+  source.connect(analyser);
+  analyser.fftSize = 256;
+
+  const data = new Uint8Array(analyser.fftSize);
+
+  function draw() {
+    requestAnimationFrame(draw);
+    analyser.getByteTimeDomainData(data);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = '#fff';
+
+    let x = 0;
+    const slice = canvas.width / data.length;
+
+    for (let i = 0; i < data.length; i++) {
+      const y = (data[i] / 128.0) * (canvas.height / 2);
+      if (i === 0) ctx.moveTo(x, y);
+      else ctx.lineTo(x, y);
+      x += slice;
+    }
+
+    ctx.stroke();
+  }
+
+  draw();
+}).catch(err => console.error(err));
+
+
+
 // Generate random number
 function getRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
@@ -20,6 +59,7 @@ recognition.start();
 function onSpeak(event) {
   const msg = event.results[0][0].transcript;  // You can log the event to view the structure of the data
 // console.log(msg);
+msgEl.innerHTML = '';
   writeMessage(msg);
   checkNumber(msg);
 }
@@ -30,6 +70,9 @@ recognition.addEventListener('result', onSpeak);
 //See in the DOM what the user has spoken
 // Write what user speaks
 function writeMessage(msg) {
+
+
+
   const div = document.createElement('div');
   div.textContent = 'You said: ';
   const span = document.createElement('span');
@@ -43,6 +86,8 @@ function writeMessage(msg) {
 function checkNumber(msg) {
   let num = Number(msg);  // 👈 This is now a let instead of const since I reassign the value below (msg = "hello world")
 
+
+  // Edge Cases
   // Update the value of num if it's a single-digit number
   if (msg === 'one' || msg === 'won') {
     num = 1;
@@ -68,6 +113,8 @@ function checkNumber(msg) {
   if (Number.isNaN(num)) {
     const div = document.createElement('div');
     div.textContent = 'That is not a valid number';
+
+    msgEl.innerHTML = '';
     msgEl.append(div);
 
     return;
@@ -79,6 +126,8 @@ function checkNumber(msg) {
   if (num < 1 || num > 100) {
     const div = document.createElement('div');
     div.textContent = 'Number must be between 1 and 100';
+
+    msgEl.innerHTML = '';
     msgEl.append(div);
   }
 
@@ -94,6 +143,8 @@ function checkNumber(msg) {
     // Add listener and handler to button
     button.addEventListener('click', () => window.location.reload());
 
+    //Clear out innerHTML of msgEL
+msgEl.innerHTML = '';
     msgEl.append(h2, button);
   } else if (num > randomNum) {
     const div = document.createElement('div');
@@ -102,8 +153,16 @@ function checkNumber(msg) {
   } else {
     // if (num < randomNum)
     const div = document.createElement('div');
-    div.textContent = 'GO HIGHER';
+    div.textContent = 'GO HIGHER'; 
     msgEl.append(div);
   }
 }
+
+// At the end of the SPeechRecognition Sevrice, start it again 
+recognition.addEventListener('end', () => recognition.start());
+
+// Add listener and handler to button
+
+
+
 
